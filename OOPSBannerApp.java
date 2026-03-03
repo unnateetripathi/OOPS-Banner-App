96ae039124cd1b73a53ed1cbf0b8f5f93a2ab07d
Your Name <you@example.com>
2026-03-03 09:20:30 +0530

    Update author name to Unnatee Tripathi

---
+++ OOPSBannerApp.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp UC1 - OOPS Banner Display Application
+ *
+ * This class demonstrates a simple Java application that displays
+ * the Object Oriented Programming System (OOPS) acronym to the console.
+ *
+ * @author Unnatee Tripathi
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    /**
+     * Entry point of the Java application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("OOPS");
+
+    }
+}
